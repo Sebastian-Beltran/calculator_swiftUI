201ede1afd43d53c0285fa0054e8586c2953d68c
Sebastian Beltran <beltran.sebastiang@gmail.com>
2025-03-03 20:27:25 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Sebastian Beltran Gonzalez on 3/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Sebastian Beltran Gonzalez on 3/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
